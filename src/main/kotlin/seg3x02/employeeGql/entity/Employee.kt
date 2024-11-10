24767ef53cc7592065ad0bba953781d120ccb93a
Sara Al Showely <salsh093@uottawa.ca>
2024-11-10 00:36:48 -0500

    Ajout du code complet de la branche server du lab 8

--- src/main/kotlin/seg3x02/employeeGql/entity/Employee.kt
+++ src/main/kotlin/seg3x02/employeeGql/entity/Employee.kt
@@ -5,12 +5,12 @@ import org.springframework.data.mongodb.core.mapping.Document
 
 @Document(collection = "employee")
 data class Employee(
-        val name: String,
-        val dateOfBirth: String,
-        val city: String,
-        val salary: Float,
-        val gender: String?,
-        val email: String?
+    var name: String,
+    var dateOfBirth: String,
+    var city: String,
+    var salary: Float,
+    var gender: String?,
+    var email: String?
 ) {
     @Id
     var id: String = ""
--- src/main/kotlin/seg3x02/employeeGql/resolvers/EmployeesResolver.kt
+++ src/main/kotlin/seg3x02/employeeGql/resolvers/EmployeesResolver.kt
@@ -1,8 +1,82 @@
 package seg3x02.employeeGql.resolvers
 
+import org.springframework.data.mongodb.core.MongoOperations
+import org.springframework.data.mongodb.core.query.Criteria
+import org.springframework.data.mongodb.core.query.Query
+import org.springframework.graphql.data.method.annotation.Argument
+import org.springframework.graphql.data.method.annotation.MutationMapping
+import org.springframework.graphql.data.method.annotation.QueryMapping
 import org.springframework.stereotype.Controller
+import seg3x02.employeeGql.entity.Employee
+import seg3x02.employeeGql.repository.EmployeesRepository
+import seg3x02.employeeGql.resolvers.types.CreateEmployeeInput
+import java.util.*
 
 @Controller
-class EmployeesResolver {
+class EmployeesResolver(
+    private val employeesRepository: EmployeesRepository,
+    private val mongoOperations: MongoOperations
+) {
+    @QueryMapping
+    fun employees(): List<Employee> {
+        return employeesRepository.findAll()
+    }
 
+    @QueryMapping
+    fun employeeById(@Argument id: String): Employee? {
+        val employee = employeesRepository.findById(id)
+        return employee.orElse(null)
+    }
+
+    @MutationMapping
+    fun newEmployee(@Argument("createEmployeeInput") input: CreateEmployeeInput): Employee {
+        if (input.name != null && input.dateOfBirth != null && input.city != null && input.salary != null) {
+            val employee = Employee(
+                name = input.name,
+                dateOfBirth = input.dateOfBirth,
+                city = input.city,
+                salary = input.salary,
+                gender = input.gender,
+                email = input.email
+            )
+            employee.id = UUID.randomUUID().toString()
+            employeesRepository.save(employee)
+            return employee
+        } else {
+            throw Exception("Invalid input")
+        }
+    }
+
+    @MutationMapping
+    fun deleteEmployee(@Argument("id") id: String): Boolean {
+        employeesRepository.deleteById(id)
+        return true
+    }
+
+    @MutationMapping
+    fun updateEmployee(@Argument id: String, @Argument("createEmployeeInput") input: CreateEmployeeInput): Employee {
+        val employee = employeesRepository.findById(id)
+        employee.ifPresent {
+            if (input.name != null) {
+                it.name = input.name
+            }
+            if (input.dateOfBirth != null) {
+                it.dateOfBirth = input.dateOfBirth
+            }
+            if (input.city != null) {
+                it.city = input.city
+            }
+            if (input.salary != null) {
+                it.salary = input.salary
+            }
+            if (input.gender != null) {
+                it.gender = input.gender
+            }
+            if (input.email != null) {
+                it.email = input.email
+            }
+            employeesRepository.save(it)
+        }
+        return employee.get()
+    }
 }
